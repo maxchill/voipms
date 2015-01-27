3284a59ede7e0a913227dcb711f527fb97f4e9ed
Thomas Levine <_@thomaslevine.com>
2015-01-27 13:01:32 +0000

    -

--- voipms.py
+++ voipms.py
@@ -1,3 +1,5 @@
+import requests
+
 def api(api_username, api_password, method, params = None):
     url = 'https://voip.ms/api/v1/rest.php'
     if params == None:
@@ -7,4 +9,14 @@ def api(api_username, api_password, method, params = None):
         'api_password': api_password,
         'method': method,
     })
-    retutrn requests.get(url, params = params)
+    response = requests.get(url, params = params)
+    if response.ok:
+        return response.json()
+
+def getSMS():
+      {'date': '2015-01-27 02:25:22',
+                 'did': '9145741328',
+                    'id': '1130177',
+                       'message': 'Hey T',
+                          'type': '1',
+                             'contact': '4158464482'},
